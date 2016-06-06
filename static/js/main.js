6315c98cf0e0c506a500471a84065a8319743f21
Jamie Robertson <jamie@jamierobertson.io>
2016-06-07 01:29:56 +0200

    Continue styling site

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,28 @@
 'use strict';
 
+var e = 'enquiry@italianencounters.com';
+
 jQuery(function($) {
 	
     var $lazyImages = $('img.lazy'), 
-    	e = 'foo@bar.com';
+    	$jsExpand = $('.jsExpand');
 
+    // Init lazyload
     $lazyImages.lazyload();
 
+    // Reveal content on 'Read more' click
+    $jsExpand.on('click', '.jsExpandTrigger', function(e) {
+    	var $container = $(e.delegateTarget);
+    	console.log($container);
+    	if ($container.hasClass('is-truncated')) {
+    		$container.removeClass('is-truncated');	
+    	} else {
+    		$container.addClass('is-truncated');
+    	}
+    	return false;
+    });
+
+    // Insert email address
     $('.jsAddContact').append('<a href="mailto:'+ e +'">' + e + '</a>');
-	
+
 });
